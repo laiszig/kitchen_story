5f7c93cc1806230d357d84e135cf193d26c9b100
laiszig <laisziegler@gmail.com>
2023-02-13 15:56:24 -0300

    write category script

--- kitchen_story_backend/scripts/scripts.sql
+++ kitchen_story_backend/scripts/scripts.sql
@@ -1,7 +1,22 @@
-CREATE
-DATABASE kitchen_story;
+# CREATE TABLE kitchen_story.item_categories (
+#     category_id INT PRIMARY KEY,
+#     category_name VARCHAR(50) NOT NULL
+# );
 
-CREATE TABLE kitchen_story.item_category (
-    category_id   INT PRIMARY KEY,
-    category_name VARCHAR(50) NOT NULL,
-);
+# INSERT INTO kitchen_story.item_categories (category_name)
+# VALUES
+#     ('Fruit'),
+#     ('Vegetables'),
+#     ('Bakery'),
+#     ('Meat'),
+#     ('Dairy'),
+#     ('Seafood'),
+#     ('Deli'),
+#     ('Beverages'),
+#     ('Snacks'),
+#     ('Cereal'),
+#     ('Canned Foods'),
+#     ('Spices & Herbs'),
+#     ('Baking & Cooking'),
+#     ('Baby & Toddler'),
+#     ('Pet Food');
