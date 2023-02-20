08b10549f97bb5f2d5634e4c29a0446543993fbd
laiszig <laisziegler@gmail.com>
2023-02-20 11:09:52 -0300

    create product list table

--- kitchen_story_frontend/src/app/product-list/product-list.component.ts
+++ kitchen_story_frontend/src/app/product-list/product-list.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  products:any = [];
+
+  ngOnInit(): void {
+    this.products = [
+      {"name" : "carrot", "category" : "vegetable", "price" : "0.99"}
+    ]
+  }
+
+  btnClick=  () => {
+    //this.router.navigate(['payment']);
+    console.log("click")
+};
+
 }
